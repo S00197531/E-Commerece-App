9cc4d877db9470330a83126deb5f2f9d20479dbd
Adam <S00197531@mail.itsligo.ie>
2023-04-05 13:47:36 +0100

    added get all tags method and get all foods by tag

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { sample_foods } from 'src/data';
+import { sample_foods, sample_tags } from 'src/data';
 import { Food } from '../shared/models/Food';
+import { Tag } from '../shared/models/tag';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,16 @@ export class FoodService {
   getAllFoodsBySearch(searchTerm:string){
     return this.getAll().filter(food=> food.name.toLowerCase().includes(searchTerm.toLowerCase()))
   }
+
+  getAllTags():Tag[]{
+    return sample_tags;
+  }
+  
+  getAllFoodsByTag(tag:string):Food[]{
+    return tag == "All"?
+    this.getAll():
+    this.getAll().filter(food => food.tags?.includes(tag));
+  }
 }
+
+
