009dd8f69e6528fad33011df8d6765bcd609db6c
Adam <S00197531@mail.itsligo.ie>
2023-04-17 11:43:47 +0100

    added search term to api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,6 +12,14 @@ app.get("/api/foods", (req,res) => {
     res.send(sample_foods);
 })
 
+app.get("/api/foods/search/:searchTerm", (req, res) => {
+    const searchTerm = req.params.searchTerm;
+    const foods = sample_foods
+    .filter(food=> food.name.toLowerCase()
+    .includes(searchTerm.toLowerCase()));
+    res.send(foods)
+})
+
 const port = 5000;
 app.listen(port, () => {
     console.log("Website served on http:localhost:" + port);
