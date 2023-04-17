40644564be92cbfd6ba5e4289954d536bab58bb6
Adam <S00197531@mail.itsligo.ie>
2023-04-17 12:50:18 +0100

    added json web token and sample users

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import { sample_foods, sample_tags, sample_users } from "./data";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -45,9 +46,22 @@ app.post("/api/users/login", (req,res) => {
         user.password === password);
 
         if(user){
-            res.send()
+            res.send(generateTokenResponse(user))
+        } else {
+            res.status(400).send("username or password is invalid")
         }
 })
+
+const generateTokenResponse = (user:any) => {
+    const token = jwt.sign({
+        email:user.email, isAdmin:user.isAdmin
+    }, "12345", {
+        expiresIn: "30d"
+    });
+
+    user.token = token;
+    return user;
+}
 
 const port = 5000;
 app.listen(port, () => {
