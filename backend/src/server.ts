fa358e978c7f98940438c2b2df3d7d12e93797ed
Adam <S00197531@mail.itsligo.ie>
2023-04-17 11:50:49 +0100

    added all food api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors"
-import { sample_foods } from "./data";
+import { sample_foods, sample_tags } from "./data";
 
 const app = express();
 app.use(cors({
@@ -18,6 +18,23 @@ app.get("/api/foods/search/:searchTerm", (req, res) => {
     .filter(food=> food.name.toLowerCase()
     .includes(searchTerm.toLowerCase()));
     res.send(foods)
+})
+
+app.get("/api/foods/tags", (req,res) => {
+    res.send(sample_tags);
+})
+
+app.get("/api/foods/tag/:tagName", (req,res) => {
+    const tagName = req.params.tagName;
+    const foods = sample_foods
+    .filter(food => food.tags?.includes(tagName));
+    res.send(foods);
+})
+
+app.get("/api/foods/:foodId", (req, res) => {
+    const foodId = req.params.foodId;
+    const food = sample_foods.find(food => food.id == foodId)
+    res.send(food);
 })
 
 const port = 5000;
