55972d74758a631ab25ab882a8eefc86c23372ee
Adam <S00197531@mail.itsligo.ie>
2023-05-10 19:31:33 +0100

    map working and showing users current location

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { LatLngTuple, Map, tileLayer, map} from 'leaflet';
+import { LatLngExpression } from 'leaflet';
+import { LatLngTuple, Map, tileLayer, map, icon, Marker, LatLng, marker} from 'leaflet';
 import { LocationService } from 'src/app/services/location.service';
 
 @Component({
@@ -9,12 +10,21 @@ import { LocationService } from 'src/app/services/location.service';
 })
 export class MapComponent implements OnInit{
 
+  private readonly MARKER_ZOOM_LEVEL = 16;
+  private readonly MARKER_ICON = icon({
+    iconUrl:
+      'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
+    iconSize: [42, 42],
+    iconAnchor: [21, 42],
+  });
   private readonly DEFAULT_LATLNG: LatLngTuple= [13.75, 21.62]
 
   @ViewChild('map', {static:true})
-  mapRef!: ElementRef
 
+  mapRef!: ElementRef
   map!:Map;
+  currentMarker!:Marker;
+
   constructor(private locationService: LocationService){}
 
   ngOnInit(): void {
@@ -35,8 +45,20 @@ export class MapComponent implements OnInit{
   findMyLocation(){
     this.locationService.getCurrentLocation().subscribe({
       next: (latlng) => {
-        console.log(latlng)
+        this.map.setView(latlng, this.MARKER_ZOOM_LEVEL)
+        this.setMarker(latlng)
       }
     })
+  }
+
+  setMarker(latlng:LatLngExpression){
+    if(this.currentMarker){
+      this.currentMarker.setLatLng(latlng);
+      return;
+    }
+    this.currentMarker = marker(latlng, {
+      draggable: true,
+      icon: this.MARKER_ICON
+    }).addTo(this.map);
   }
 }
