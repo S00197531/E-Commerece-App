af2d5069805295d01623679bc61b6026ec3a675e
Adam <S00197531@mail.itsligo.ie>
2023-05-10 19:06:56 +0100

    added map with  leaflet

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
 import { MapComponent } from './components/partials/map/map.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 
 @NgModule({
@@ -57,6 +58,7 @@ import { MapComponent } from './components/partials/map/map.component';
     AppRoutingModule,
     RatingModule,
     HttpClientModule,
+    LeafletModule,
     ReactiveFormsModule,
     ToastrModule.forRoot({
       timeOut:3000,
