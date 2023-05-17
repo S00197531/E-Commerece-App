85aa8638c7852bc4332344d6370fa34765a4fa86
Adam <S00197531@mail.itsligo.ie>
2023-05-17 10:47:11 +0100

    cors update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin:["http://localhost:5000"]
 }));
 
 app.use("/api/foods", foodRouter)
