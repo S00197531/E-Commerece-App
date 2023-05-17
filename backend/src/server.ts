87420da51b11a657648477c34f395cb358726e03
Adam <S00197531@mail.itsligo.ie>
2023-05-17 13:40:24 +0100

    heroku restart

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,19 +15,14 @@ dbConnect();
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    credentials:true,
-    origin:["https://eat-more.herokuapp.com"],
-    allowedHeaders: ['access_token'],
-    methods: ['GET, POST, PUT, DELETE, OPTIONS']
-}));
+app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://eat-more.herokuapp.com');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, access_token');
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://eat-more.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, access_token');
+    next();
+  });
 
 
 app.use("/api/foods", foodRouter)
