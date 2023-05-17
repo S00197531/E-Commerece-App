f6c27c01b73720df9488236d896ddf9474994d46
Adam <S00197531@mail.itsligo.ie>
2023-05-17 12:23:37 +0100

    options header

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://eat-more.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, access_token');
     next();
   });
