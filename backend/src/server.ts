20c0c1b16650f5a256cee85cf752c7a3351f50e3
Adam <S00197531@mail.itsligo.ie>
2023-05-17 13:43:15 +0100

    heroku restart

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://eat-more.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, access_token');
+    
     next();
   });
 
