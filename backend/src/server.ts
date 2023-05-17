1e6f9286d04bf597c0123b22914ed38c9d04570c
Adam <S00197531@mail.itsligo.ie>
2023-05-17 11:58:51 +0100

    access token header added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://eat-more.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, access_token');
     next();
   });
 
